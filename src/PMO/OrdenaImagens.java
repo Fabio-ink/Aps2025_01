00dbcd30535e961bab7ae1baa3fbe9fff38bd19a
Fabio <fabiopradaa@gmail.com>
2025-05-01 20:08:32 -0300

    Primeiro método de ordenação

--- src/PMO/OrdenaImagens.java
+++ src/PMO/OrdenaImagens.java
@@ -3,29 +3,38 @@ package PMO;
 import java.io.File;
 import java.util.Arrays;
 
-public class OrdenaArray {
+public class OrdenaImagens {
     public static void main(String[] args) {
         File pasta = new File("/home/fluxyfp/Downloads/PastaTeste");
 
         // Filtro: apenas arquivos de imagem
         File[] imagens = pasta.listFiles((dir, nome) ->
-                nome.toLowerCase().endsWith(".jpg")
-                nome.toLowerCase().endsWith(".jpeg")
-                nome.toLowerCase().endsWith(".jpg")
+                nome.toLowerCase().endsWith(".jpg") || nome.toLowerCase().endsWith(".jpeg") || nome.toLowerCase().endsWith(".png")
         );
 
 
         if (imagens != null) {
+            // Início da medição
+            long inicio = System.nanoTime();
+
             // Ordena por nome (alfabético)
             Arrays.sort(imagens, (f1, f2) -> Long.compare(f1.length(), f2.length()));
 
-            // Exibe os nomes dos arquivos ordenados
+            // Fim da medição
+            long fim = System.nanoTime();
+
+            // Tempo em milissegundos
+            long duracaoMs = (fim - inicio) / 1_000_000;
+
             for (File imagem : imagens) {
                 System.out.println(imagem.getName());
                 System.out.println(imagem.length());
             }
+            // Exibe resultado
+            System.out.println("Tempo de ordenação: " + duracaoMs + " ms");
         } else {
             System.out.println("A pasta está vazia ou não existe.");
+
         }
     }
 }
