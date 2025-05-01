99a5c7335e1a55385321a8bcbd5491e5599f11d7
Fabio <fabiopradaa@gmail.com>
2025-05-01 20:38:29 -0300

    Terceiro método de ordenação

--- src/TMO/BublesortImagens.java
+++ src/TMO/BublesortImagens.java
@@ -1,4 +1,22 @@
 package TMO;
 
-public class BublesortImagens {
+// Implementação do Bubble Sort
+public static void bubbleSort(File[] arquivos) {
+    int n = arquivos.length;
+    boolean trocou;
+
+    for (int i = 0; i < n - 1; i++) {
+        trocou = false;
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arquivos[j].getName().compareToIgnoreCase(arquivos[j + 1].getName()) > 0) {
+                // Troca os arquivos de posição
+                File temp = arquivos[j];
+                arquivos[j] = arquivos[j + 1];
+                arquivos[j + 1] = temp;
+                trocou = true;
+            }
+        }
+        // Se não houve troca, a lista já está ordenada
+        if (!trocou) break;
+    }
 }
