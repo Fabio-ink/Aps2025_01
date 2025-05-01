75bf549a7c74bb10e0c63de5d272e8666df692a9
Fabio <fabiopradaa@gmail.com>
2025-05-01 19:31:54 -0300

    primeiro método de ordenação

---
+++ src/OrdenaImagens.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.util.Arrays;
+
+public class OrdenaImagens {
+    public static void main(String[] args) {
+        File pasta = new File("/home/fluxyfp/Downloads/PastaTeste");
+
+        // Filtro: apenas arquivos de imagem
+        File[] imagens = pasta.listFiles((dir, nome) ->
+                nome.toLowerCase().endsWith(".jpg"));
+
+
+        if (imagens != null) {
+            // Ordena por nome (alfabético)
+            Arrays.sort(imagens, (f1, f2) -> Long.compare(f1.length(), f2.length()));
+
+            // Exibe os nomes dos arquivos ordenados
+            for (File imagem : imagens) {
+                System.out.println(imagem.getName());
+                System.out.println(imagem.length());
+            }
+        } else {
+            System.out.println("A pasta está vazia ou não existe.");
+        }
+    }
+}
